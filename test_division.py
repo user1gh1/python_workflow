4155f92cb5b59726c9316a9d54ec049631894294
user1gh1 <user1gm1@gmail.com>
2023-08-10 01:49:42 +0400

    add interactive test

--- test_division.py
+++ test_division.py
@@ -1,15 +1,19 @@
 import unittest
 import os
-# import yaml
+from division import *
 
-# x = yaml.load("""a: 45\nb: cheese\nc: 7.8""")
+# import yaml
+#x = yaml.load("""a: 45\nb: cheese\nc: 7.8""")
+
 number1 = os.environ.get('number1')
 number2 = os.environ.get('number2')
 answer = os.environ.get('answer')
 
 print (number1)
+number1 = os.getenv("number1")
+print (number1)
 
-from division import *
+
 
 class DivisionTest(unittest.TestCase):
     def test_division(self):
